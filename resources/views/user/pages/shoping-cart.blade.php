a435c615e76c9bc1722d8fddbfff79a123744ebf
Ryaannn <144214893+Ryaannnbb@users.noreply.github.com>
2023-11-27 09:38:23 +0700

    memperbaiki halaman pesanan

--- resources/views/user/pages/shoping-cart.blade.php
+++ resources/views/user/pages/shoping-cart.blade.php
@@ -1,7 +1,7 @@
 @extends('user.layouts.main')
 
 @section('content')
-    
+
     <!-- Hero Section Begin -->
     {{-- <section class="hero hero-normal">
         <div class="container">
@@ -86,10 +86,13 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                @php
+                                    $subtotal = 0;
+                                @endphp
                                 @foreach ($pesanans as $pesanan)
                                 <tr>
                                     <td class="shoping__cart__item">
-                                        <img src="img/cart/cart-1.jpg" alt="">
+                                        <img src="{{ asset($pesanan->produk->path_produk) }}" alt="{{ $pesanan->produk->nama_produk }}">
                                         <h5>{{ $pesanan->produk->nama_produk }}</h5>
                                     </td>
                                     <td class="shoping__cart__price">
@@ -98,12 +101,12 @@
                                     <td class="shoping__cart__quantity">
                                         <div class="quantity">
                                             <div class="pro-qty">
-                                                <input type="text" value="1">
+                                                <input type="text" value="{{ $pesanan->jumlah }}" readonly>
                                             </div>
                                         </div>
                                     </td>
                                     <td class="shoping__cart__total">
-                                        Rp. {{ number_format($pesanan->total, 0, ',', '.') }}
+                                        Rp. {{ number_format($pesanan->produk->harga * $pesanan->jumlah, 0, ',', '.') }}
                                     </td>
                                     <td class="shoping__cart__item__close">
                                         <form action="{{ route('cart.destroy', $pesanan->id) }}" method="POST">
@@ -113,6 +116,9 @@
                                         </form>
                                     </td>
                                 </tr>
+                                @php
+                                    $subtotal += $pesanan->produk->harga * $pesanan->jumlah;
+                                @endphp
                                 @endforeach
                             </tbody>
                         </table>
@@ -122,12 +128,12 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="shoping__cart__btns">
-                        <a href="#" class="primary-btn cart-btn">CONTINUE SHOPPING</a>
-                        <a href="#" class="primary-btn cart-btn cart-btn-right"><span class="icon_loading"></span>
+                        <a href="{{ route('shop.index') }}" class="custom-btn cart-btn-continue-shopping primary-btn cart-btn">CONTINUE SHOPPING</a>
+                        <a href="#" class="custom-btn cart-btn-update primary-btn cart-btn cart-btn-right"><span class="icon_loading"></span>
                             Upadate Cart</a>
                     </div>
                 </div>
-                <div class="col-lg-6">
+                {{-- <div class="col-lg-6">
                     <div class="shoping__continue">
                         <div class="shoping__discount">
                             <h5>Discount Codes</h5>
@@ -137,13 +143,13 @@
                             </form>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-6">
+                </div> --}}
+                <div class="col-lg-12">
                     <div class="shoping__checkout">
                         <h5>Cart Total</h5>
                         <ul>
-                            <li>Subtotal <span>$454.98</span></li>
-                            <li>Total <span>$454.98</span></li>
+                            <li>Total Quantity <span>{{ $pesanans->sum('jumlah') }}</span></li>
+                            <li>Total <span>Rp. {{ number_format($subtotal, 0, ',', '.') }}</span></li>
                         </ul>
                         <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
                     </div>
